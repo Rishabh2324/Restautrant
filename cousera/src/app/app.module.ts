aaf7cc7f6acef64f8e9316ec0ab4d6fe209ce891
Rishabh2324 <rishabh2401jain@gmail.com>
2019-10-06 01:13:11 +0530

    added form validations

--- cousera/src/app/app.module.ts
+++ cousera/src/app/app.module.ts
@@ -1,16 +1,15 @@
 
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { MatCheckboxModule} from '@angular/material';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatToolbarModule} from '@angular/material' ;
-import {MatGridListModule} from '@angular/material/grid-list';
+import { MatToolbarModule } from '@angular/material';
+import { MatGridListModule } from '@angular/material/grid-list';
 import 'hammerjs';
 import { FlexLayoutModule } from '@angular/flex-layout';
-import {MatCardModule} from '@angular/material/card';
-import {MatButtonModule} from '@angular/material/button';
-import {MatListModule} from '@angular/material/list';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
 import { MenuComponent } from './menu/menu.component';
 import { DishdetailsComponent } from './dishdetails/dishdetails.component'
 import { HeaderComponent } from './header/header.component';
@@ -18,10 +17,16 @@ import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ContactUsComponent } from './contact-us/contact-us.component';
-import {DishService} from './services/dish.service';
-import {PromotionService} from './services/promotion.service';
+import { DishService } from './services/dish.service';
+import { PromotionService } from './services/promotion.service';
 import { LeaderService } from './services/leader.service';
-import {AppRoutingModule} from './app-routing/app-routing.module';
+import { AppRoutingModule } from './app-routing/app-routing.module';
+import { LoginComponent } from './login/login.component';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule } from '@angular/forms'
+import { MatCheckboxModule } from '@angular/material/checkbox';
 
 
 @NgModule({
@@ -33,8 +38,9 @@ import {AppRoutingModule} from './app-routing/app-routing.module';
     FooterComponent,
     HomeComponent,
     AboutComponent,
-    ContactUsComponent
-   ],
+    ContactUsComponent,
+    LoginComponent
+  ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
@@ -45,14 +51,23 @@ import {AppRoutingModule} from './app-routing/app-routing.module';
     MatGridListModule,
     MatCardModule,
     MatListModule,
+    MatDialogModule,
     AppRoutingModule,
-    
+    MatFormFieldModule,
+    MatCheckboxModule,
+    MatInputModule,
+    FormsModule
+
   ],
   providers: [
     DishService,
     PromotionService,
     LeaderService
 
+  ],
+
+  entryComponents: [
+    LoginComponent
   ],
   bootstrap: [AppComponent]
 })
