3444d1419840c4e4e005d46426158cc809997e99
Rishabh2324 <rishabh2401jain@gmail.com>
2019-12-03 00:44:56 +0530

    DEPLOYED TO HEROKUAPP

--- cousera/server.js
+++ cousera/server.js
@@ -1,6 +1,26 @@
-app.listen(process.env.PORT);
-let port = process.env.PORT;
-if (port == null || port == "8000") {
-  port = 8000;
-}
-app.listen(port);
+const express = require('express');
+const path = require('path');
+var bodyParser = require('body-parser');
+const app = express();
+
+var cors = require('cors');
+
+const PORT = process.env.PORT || 5000;
+
+//Cross origin support middle
+app.use(cors());
+//body-parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/cousera/'));
+app.get('/*', function(req, res) {
+
+    res.sendFile(path.join(__dirname + '/dist/cousera/index.html'));
+});
+
+
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}...`);
+});
